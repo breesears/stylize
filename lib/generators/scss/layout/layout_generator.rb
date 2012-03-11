125a7061a1c01602b713c83a2bea012909f587a6
Bree Sears <blynn78@gmail.com>
2012-03-10 17:46:15 -0800

    testing blue option

--- lib/generators/scss/layout/layout_generator.rb
+++ lib/generators/scss/layout/layout_generator.rb
@@ -8,9 +8,9 @@ module Scss
 
       def create_layout
         template 'layout.html.erb', "app/views/layouts/application.html.erb"
-        copy_file "#{file_name}/#{color_name}.css", "public/stylesheets/#{file_name}/#{color_name}.css"
+        copy_file "#{file_name}/blue/blue.css", "public/stylesheets/#{file_name}/blue/blue.css"
         copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
-        copy_file "#{file_name}/#{color_name}_bg.gif", "public/images/#{file_name}/#{color_name}_bg.gif"
+        copy_file "#{file_name}/blue/blue_bg.gif", "public/images/#{file_name}/blue/blue_bg.gif"
       end
 
       private
