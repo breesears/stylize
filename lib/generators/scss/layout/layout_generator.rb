7af88b8a089135242acaf99e5ba6a2a0af554c3e
Bree Sears <blynn78@gmail.com>
2012-03-18 15:41:50 -0700

    removed detection

--- lib/generators/scss/layout/layout_generator.rb
+++ lib/generators/scss/layout/layout_generator.rb
@@ -8,14 +8,9 @@ module Scss
 
       def create_layout
         template 'layout.html.erb', "app/views/layouts/application.html.erb"
-        
-        if (Rails::VERSION::STRING == 3.1)
-          copy_file "#{file_name}/#{file_name}.css", "app/assets/stylesheets/#{file_name}/#{file_name}.css"
-        else  
          copy_file "#{file_name}/#{file_name}.css", "public/stylesheets/#{file_name}/#{file_name}.css"
          copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
          copy_file "#{file_name}/#{file_name}_bg.gif", "public/images/#{file_name}/#{file_name}_bg.gif"
-       end
       end
 
       private
