8cdb20aad3d6c73b04ba616f8a00ca9fa8349148
Bree Sears <blynn78@gmail.com>
2012-03-18 13:35:04 -0700

    detection again

--- lib/generators/scss/layout/layout_generator.rb
+++ lib/generators/scss/layout/layout_generator.rb
@@ -12,9 +12,10 @@ module Scss
         if (Rails::VERSION::STRING == 3.1)
           copy_file "#{file_name}/#{file_name}.css", "app/assets/stylesheets/#{file_name}/#{file_name}.css"
         else  
-        copy_file "#{file_name}/#{file_name}.css", "public/stylesheets/#{file_name}/#{file_name}.css"
-        copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
-        copy_file "#{file_name}/#{file_name}_bg.gif", "public/images/#{file_name}/#{file_name}_bg.gif"
+         copy_file "#{file_name}/#{file_name}.css", "public/stylesheets/#{file_name}/#{file_name}.css"
+         copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
+         copy_file "#{file_name}/#{file_name}_bg.gif", "public/images/#{file_name}/#{file_name}_bg.gif"
+       end
       end
 
       private
