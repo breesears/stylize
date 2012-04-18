4cf031d17e0ea92202d7b66185f6a6b06649e16f
Bree Sears <blynn78@gmail.com>
2012-04-18 10:59:19 -0700

    testing element generator

---
+++ lib/generators/scss/element/element_generator.rb
@@ -0,0 +1,23 @@
+require 'generators/scss'
+
+module Scss
+  module Generators
+    class ElementGenerator < Base
+      include Rails::Generators
+      
+      argument :element_name, :type => :string, :banner => 'element_name'
+      argument :css_name,     :type => :string, :banner => 'css_name'
+      agrument :f_name,       :type => :string, :banner => 'f_name'
+      agrument :text_name,    :type => :string, :banner => 'text_name' :default => ""
+      
+      def add_element
+        inject_into_class "app/views/#{file_name}.html.erb", "<#{element_name} = #{css_class}>#{text_name}</#{element_name}"
+      end
+        
+      def file_name
+        f_name.underscore
+      end 
+      
+      def css_class
+        css_name.underscore
+      end
