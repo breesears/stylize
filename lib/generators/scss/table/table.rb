c5acd1a9a9ca4baec687b79423be4f462fc02f07
Bree Sears <blynn78@gmail.com>
2012-04-22 12:01:40 -0700

    created element folders

---
+++ lib/generators/scss/table/table.rb
@@ -0,0 +1,43 @@
+require 'generators/scss'
+
+module Scss
+  module Generators
+    class TableGenerator < Base
+      include Rails::Generators
+      
+      argument :f_name,       :type => :string,                     :banner => 'f_name'
+      argument :rows,         :type => :string,   :default => '0',  :banner => 'rows'
+      argument :cols,         :type => :string,   :default => '0',  :banner => 'cols'
+      
+      append_file "app/views/#{file_name}.html.erb",  rows_cols
+          
+      
+      def rows_cols
+        str = "<table>"
+        r.times do |r|
+          str << "<tr>\n"
+          c.times do |c|
+            str << "<td>&nbsp;</td>\n"
+          end
+          str << "</tr>"
+        end
+        str << "</table>"
+        return str
+      end
+     
+      def r
+        rows.to_i
+      end
+      
+      def c
+        cols.to_i
+      end
+        
+      def file_name
+        f_name.underscore
+      end 
+      
+     
+    end
+  end
+end
