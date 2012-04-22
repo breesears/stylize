189b8737069897a755b1997bd58c7f8c39651f4d
Bree Sears <blynn78@gmail.com>
2012-04-22 12:38:50 -0700

    fixed list generator

--- lib/generators/scss/list/list_generator.rb
+++ lib/generators/scss/list/list_generator.rb
@@ -6,28 +6,27 @@ module Scss
       include Rails::Generators
       
       argument :f_name,        :type => :string,                      :banner => 'f_name'
-      argument :list_type,     :type => :string,   :default => 'u',   :banner => 'list_type'
-      argument :num_of_items,  :type => :string,   :default => '0',   :banner => 'num_of_items'
+      argument :list_type,     :type => :string,                      :banner => 'list_type'
       argument :text,          :type => :array,                       :banner => 'text'
       
       append_file "app/views/#{file_name}.html.erb",  list_items
           
       
       def list_items
-        if (list_type == 'u') 
-          str = "<ul>"
-        else
+        if (list_type == 'o') 
           str = "<ol>"
+        else
+          str = "<ul>"
         end
         
         text.each do |t|
           str << "<li>t</li>\n"
         end
         
-        if (list_type == 'u')  
-          str << "</ul>"
-        else
+        if (list_type == 'o')  
           str << "</ol>"
+        else
+          str << "</ul>"
         end
       end
      
