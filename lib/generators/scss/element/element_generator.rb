9ccfbde94ce8bc89713b9ff667e7a239c49e989b
Bree Sears <blynn78@gmail.com>
2012-04-23 11:29:07 -0700

    testing again

--- lib/generators/scss/element/element_generator.rb
+++ lib/generators/scss/element/element_generator.rb
@@ -22,35 +22,35 @@ module Scss
         if (element_name == ('p' || 'div'))
                
           append_file "app/views/#{file_name}.html.erb", "<#{element} class=\"#{css_class}\">#{text_name}</#{element}>"
-        else
-          if (element_name == 'table')
-              argument :rows,         :type => :string,   :default => '0',  :banner => 'rows'
-              argument :cols,         :type => :string,  :default => '0',  :banner => 'cols'
-           append_file "app/views/#{file_name}.html.erb",  rows_cols
-          end 
+        # else
+        #         if (element_name == 'table')
+        #             argument :rows,         :type => :string,   :default => '0',  :banner => 'rows'
+        #             argument :cols,         :type => :string,  :default => '0',  :banner => 'cols'
+        #          append_file "app/views/#{file_name}.html.erb",  rows_cols
+        #         end 
         end
       end
       
-      def rows_cols
-        str = "<table>"
-        r.times do |r|
-          str << "<tr>\n"
-          c.times do |c|
-            str << "<td>&nbsp;</td>\n"
-          end
-          str << "</tr>"
-        end
-        str << "</table>"
-        return str
-      end
-     
-      def r
-        rows.to_i
-      end
-      
-      def c
-        cols.to_i
-      end
+      # def rows_cols
+      #        str = "<table>"
+      #        r.times do |r|
+      #          str << "<tr>\n"
+      #          c.times do |c|
+      #            str << "<td>&nbsp;</td>\n"
+      #          end
+      #          str << "</tr>"
+      #        end
+      #        str << "</table>"
+      #        return str
+      #      end
+      #     
+      #      def r
+      #        rows.to_i
+      #      end
+      #      
+      #      def c
+      #        cols.to_i
+      #      end
         
       def file_name
         f_name.underscore
